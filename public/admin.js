23d381dd0aae2adcc0515cb93a20adeead5cc5fc
Musa-long <balarahinat2530@gmail.com>
2026-09-10 01:23:44 +0100

    Delete public/admin.js

--- public/admin.js
+++
@@ -1,8 +0,0 @@
-function login(){
-    const pass = document.getElementById('password').value;
-    if(pass === 'Musa2530'){
-        document.getElementById('orders').innerHTML = '<h2>Welcome Admin!</h2><p>Orders will appear here</p>';
-    } else {
-        alert('Wrong Password!');
-    }
-}
